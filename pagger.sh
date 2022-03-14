29e65eef17fe8bb2d530b916a10b7055a0605b99
DylanEncode <96323298+DylanEncode@users.noreply.github.com>
2022-03-13 23:34:06 -0300

    Create pagger.sh

---
+++ pagger.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+clear
+echo "scanning pag"
+echo "autor dylan"
+echo "ej de dominio: tiktok.com"
+read -p "ingrese el dominio: " x
+sleep 1;
+echo "espere un momento"
+sleep 2;
+echo "========================"
+whois $x
+echo ""
+echo ""
+echo ""
+echo "whois obtenido correctamente."
+echo "========================"
+sleep 1;
+echo "obteniendo proximo resultado..."
+sleep 3;
+echo "resultado obtenido"
+echo "no tiene instalado el paquete, yo lo hago por ti"
+pkg install dnsutils
+sleep 1;
+echo "paquete instalado"
+sleep 1;
+echo "==========================="
+nslookup $x
+echo ""
+echo ""
+nslookup $x
+echo ""
+echo ""
+echo ""
